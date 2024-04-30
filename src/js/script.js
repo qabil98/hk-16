97448e69e7a2e1de7f5c0145e1da28fc01f86a84
qabil98 <qedimaliyev1998@gmail.com>
2024-04-30 11:47:32 +0400

    js-first

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+
+var ad=prompt("Adinizi daxil edin:");
+var soyad=prompt("Soyadinizi daxil edin")
+ var dogum=prompt("Dogum tarixiniz daxil edin:");
+
+ console.log(ad);
+ console.log(soyad);
+ console.log(dogum);
+
+ var age=2024 - dogum;
+ console.log(age);
+
+
+ alert(ad,soyad,age);
+ alert(soyad);
+ alert(age);
+
+
+
+
